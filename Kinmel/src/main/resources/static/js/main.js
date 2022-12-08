c8da79f2aeca6f724cabfc94b31ac9c9bbbee04f
Sahar3ha <saharsha.xtha.3@gmail.com>
2022-12-08 17:06:29 +0545

    added js

---
+++ Kinmel/src/main/resources/static/js/main.js
@@ -0,0 +1,251 @@
+(function ($) {
+ "use strict";
+
+/*----------------------------
+ jQuery MeanMenu
+------------------------------ */
+	$('nav#dropdown').meanmenu({
+		meanScreenWidth: "991",
+		meanMenuContainer: ".mobile-menu-area .container",
+	});
+
+// mainSlider
+function mainSlider() {
+	var BasicSlider = $('.slider-active');
+	BasicSlider.on('init', function (e, slick) {
+		var $firstAnimatingElements = $('.single-slider:first-child').find('[data-animation]');
+		doAnimations($firstAnimatingElements);
+	});
+	BasicSlider.on('beforeChange', function (e, slick, currentSlide, nextSlide) {
+		var $animatingElements = $('.single-slider[data-slick-index="' + nextSlide + '"]').find('[data-animation]');
+		doAnimations($animatingElements);
+	});
+	BasicSlider.slick({
+		autoplay: false,
+		autoplaySpeed: 10000,
+		dots: true,
+		fade: true,
+		prevArrow: '<button type="button" class="slick-prev"><i class="icofont icofont-long-arrow-left"></i></button>',
+		nextArrow: '<button type="button" class="slick-next"><i class="icofont icofont-long-arrow-right"></i></button>',
+		arrows: true,
+		responsive: [
+			{ breakpoint: 767, settings: { dots: false, arrows: false } }
+		]
+	});
+
+	function doAnimations(elements) {
+		var animationEndEvents = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+		elements.each(function () {
+			var $this = $(this);
+			var $animationDelay = $this.data('delay');
+			var $animationType = 'animated ' + $this.data('animation');
+			$this.css({
+				'animation-delay': $animationDelay,
+				'-webkit-animation-delay': $animationDelay
+			});
+			$this.addClass($animationType).one(animationEndEvents, function () {
+				$this.removeClass($animationType);
+			});
+		});
+	}
+}
+mainSlider();
+
+
+/*----------------------------
+ wow js active
+------------------------------ */
+	new WOW().init();
+
+/*----------------------------
+ owl active
+------------------------------ */
+  $(".product-curosel").owlCarousel({
+      autoPlay: false,
+	  slideSpeed:3000,
+	  pagination:false,
+	  navigation:false,
+      items : 4,
+	  /* transitionStyle : "fade", */    /* [This code for animation ] */
+	  navigationText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+      itemsDesktop : [1199,4],
+	  itemsDesktopSmall : [980,3],
+	  itemsTablet: [767,1],
+	  itemsMobile : [479,1],
+  });
+
+/*----------------------------
+ owl active
+------------------------------ */
+  $(".upcoming-curosel").owlCarousel({
+      autoPlay: false,
+	  slideSpeed:3000,
+	  pagination:true,
+	  navigation:false,
+      items : 1,
+	  /* transitionStyle : "fade", */    /* [This code for animation ] */
+	  navigationText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+      itemsDesktop : [1199,1],
+	  itemsDesktopSmall : [980,1],
+	  itemsTablet: [768,1],
+	  itemsMobile : [479,1],
+  });
+/*----------------------------
+ owl active
+------------------------------ */
+  $(".blog-curosel").owlCarousel({
+      autoPlay: false,
+	  slideSpeed:3000,
+	  pagination:false,
+	  navigation:false,
+      items : 3,
+	  navigationText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+      itemsDesktop : [1199,3],
+	  itemsDesktopSmall : [980,3],
+	  itemsTablet: [768,1],
+	  itemsMobile : [479,1],
+  });
+
+/*----------------------------
+ owl active
+------------------------------ */
+  $(".brand-curosel").owlCarousel({
+      autoPlay: false,
+	  slideSpeed:3000,
+	  pagination:false,
+	  navigation:false,
+      items : 6,
+	  navigationText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+      itemsDesktop : [1199,5],
+	  itemsDesktopSmall : [980,4],
+	  itemsTablet: [768,2],
+	  itemsMobile : [479,1],
+  });
+
+/*----------------------------
+ owl active
+------------------------------ */
+  $(".related-curosel").owlCarousel({
+      autoPlay: false,
+	  slideSpeed:3000,
+	  pagination:false,
+	  navigation:false,
+      items : 4,
+	  navigationText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+      itemsDesktop : [1199,3],
+	  itemsDesktopSmall : [980,3],
+	  itemsTablet: [768,2],
+	  itemsMobile : [479,1],
+  });
+/*----------------------------
+ owl active
+------------------------------ */
+  $(".testimonial-carousel").owlCarousel({
+      autoPlay: false,
+	  slideSpeed:3000,
+	  pagination:true,
+	  navigation:false,
+      items : 1,
+	  /* transitionStyle : "fade", */    /* [This code for animation ] */
+	  navigationText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+	  singleItem: true,
+  });
+/*----------------------------
+ owl active
+------------------------------ */
+  $(".sale-off-carosel").owlCarousel({
+      autoPlay: true,
+	  slideSpeed:3000,
+	  pagination:false,
+	  navigation:false,
+      items : 1,
+	  /* transitionStyle : "fade", */    /* [This code for animation ] */
+	  navigationText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+	  singleItem: true,
+  });
+
+/*----------------------------
+ price-slider active
+------------------------------ */
+	  $( "#slider-range" ).slider({
+	   range: true,
+	   min: 40,
+	   max: 600,
+	   values: [ 60, 570 ],
+	   slide: function( event, ui ) {
+		$( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+	   }
+	  });
+	  $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+	   " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+
+/*--------------------------
+ scrollUp
+---------------------------- */
+	$.scrollUp({
+        scrollText: '<i class="fa fa-angle-up"></i>',
+        easingType: 'linear',
+        scrollSpeed: 900,
+        animation: 'fade'
+    });
+
+/*---------------------
+ countdown
+--------------------- */
+	$('[data-countdown]').each(function() {
+	  var $this = $(this), finalDate = $(this).data('countdown');
+	  $this.countdown(finalDate, function(event) {
+		$this.html(event.strftime('<span class="cdown days"><span class="time-count">%-D</span> <p>Days</p></span> <span class="cdown hour"><span class="time-count">%-H</span> <p>Hour</p></span> <span class="cdown minutes"><span class="time-count">%M</span> <p>Min</p></span> <span class="cdown second"> <span><span class="time-count">%S</span> <p>Sec</p></span>'));
+	  });
+	});
+
+
+	/*----- cart-plus-minus-button -----*/
+	 $(".cart-plus-minus").append('<div class="dec qtybutton">-</div><div class="inc qtybutton">+</div>');
+	  $(".qtybutton").on("click", function() {
+		var $button = $(this);
+		var oldValue = $button.parent().find("input").val();
+		if ($button.text() == "+") {
+		  var newVal = parseFloat(oldValue) + 1;
+		} else {
+		   // Don't allow decrementing below zero
+		  if (oldValue > 0) {
+			var newVal = parseFloat(oldValue) - 1;
+			} else {
+			newVal = 0;
+		  }
+		  }
+		$button.parent().find("input").val(newVal);
+	  });
+
+
+/*-------------------------
+  showlogin toggle function
+--------------------------*/
+	 $( '#showlogin' ).on('click', function() {
+        $( '#checkout-login' ).slideToggle(900);
+     });
+
+/*-------------------------
+  showcoupon toggle function
+--------------------------*/
+	 $( '#showcoupon' ).on('click', function() {
+        $( '#checkout_coupon' ).slideToggle(900);
+     });
+
+/*-------------------------
+  Create an account toggle function
+--------------------------*/
+	 $( '#cbox' ).on('click', function() {
+        $( '#cbox_info' ).slideToggle(900);
+     });
+
+/*-------------------------
+  Create an account toggle function
+--------------------------*/
+	 $( '#ship-box' ).on('click', function() {
+        $( '#ship-box-info' ).slideToggle(1000);
+     });
+
+
+})(jQuery);
