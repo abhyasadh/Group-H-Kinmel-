a789d8f3854518cfc79d7ccefec3cf32ab11d272
Swopnil38 <swopnilbista2003@gmail.com>
2023-02-23 08:58:03 +0545

    Created All Logic to retrieve Data for Dashboard

--- Kinmel/src/main/java/com/system/kinmel/repo/CartRepo.java
+++ Kinmel/src/main/java/com/system/kinmel/repo/CartRepo.java
@@ -26,4 +26,43 @@ public interface CartRepo extends JpaRepository<Cart, Integer> {
     @Query(value = "SELECT user_id, COUNT(*) as count FROM Cart GROUP BY user_id",nativeQuery = true)
     List<Object[]> fetchAllCustomer();
 
+
+    @Query(value = "SELECT SUM(c.quantity * p.product_price) AS weekly_sales FROM cart c JOIN products p ON c.product_id = p.id WHERE c.created_date >= NOW() - INTERVAL '7 weeks' GROUP BY DATE_TRUNC('week', c.created_date) ORDER BY DATE_TRUNC('week', c.created_date) DESC",nativeQuery = true)
+    List<Integer> fetchlast5weeksales();
+
+//    @Query(nativeQuery = true, value = "SELECT COALESCE(SUM(c.quantity * p.product_price), 0) AS daily_sales, d.sale_date "
+//            + "FROM (SELECT generate_series(NOW() - INTERVAL '1 week', NOW(), '1 day')::date AS sale_date) d "
+//            + "LEFT JOIN cart c ON DATE_TRUNC('day', c.created_date) = d.sale_date "
+//            + "LEFT JOIN products p ON c.product_id = p.id "
+//            + "GROUP BY d.sale_date ORDER BY d.sale_date DESC")
+//    List<Object[]> getDailySalesLastWeek();
+
+    @Query(nativeQuery = true, value = "SELECT COALESCE(SUM(c.quantity * p.product_price), 0) AS daily_sales, d.sale_date "
+            + "FROM (SELECT generate_series(NOW() - INTERVAL '1 week', NOW(), '1 day') AS sale_date) d "
+            + "LEFT JOIN cart c ON DATE_TRUNC('day', c.created_date) = CAST(d.sale_date AS DATE) "
+            + "LEFT JOIN products p ON c.product_id = p.id "
+            + "GROUP BY d.sale_date ORDER BY d.sale_date DESC")
+    List<Object[]> getDailySalesLastWeek();
+
+
+
+
+    @Query(nativeQuery = true, value = "SELECT p.id AS product_id, p.product_name, COALESCE(SUM(c.quantity), 0) AS quantity_sold "
+            + "FROM products p "
+            + "LEFT JOIN cart c ON c.product_id = p.id AND DATE_TRUNC('week', c.created_date) = DATE_TRUNC('week', NOW() - INTERVAL '1 week') "
+            + "GROUP BY p.id, p.product_name "
+            + "ORDER BY quantity_sold DESC")
+    List<Object[]> getProductSalesLastWeek();
+
+    @Query(nativeQuery = true, value = "SELECT c.status, COUNT(*) AS status_count "
+            + "FROM cart c "
+            + "WHERE c.created_date >= NOW() - INTERVAL '1 week' "
+            + "GROUP BY c.status")
+    List<Object[]> getStatusCountLastWeek();
+
+
+
+
+
+
 }
